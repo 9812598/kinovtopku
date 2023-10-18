27d1db7a8bf7441f79b93a276c17643db59a064f
9812598 <9812598@gmail.com>
2023-10-18 20:42:41 +0300

    start my app

---
+++ src/components/MainCard.tsx
@@ -0,0 +1,15 @@
+﻿import React from "react";
+
+type Props = {};
+
+export default function MainCard({}: Props) {
+  return (
+    <>
+      <div className="flex flex-col gap-3 border-cyan-500 border-solid border-2 rounded-lg px-5 py-3 items-center bg-slate-300 hover:bg-slate-400 ">
+        <h2>Name of the film</h2>
+        <h4 className="text-base">26.11.1988</h4>
+        <p className="font-normal text-base">Краткое описание ascasc ascasc</p>
+      </div>
+    </>
+  );
+}
